9e9ef6ef3dec3cc3cbe44d849d49d762485e632c
Nick <nick@jackrussellsoftware.com>
2011-06-06 14:45:23 -0400

    Growl notifications showing up.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'rest-client'
 gem 'crack'
+gem 'growl'
--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,18 @@
 require 'rest-client'
 require 'crack'
+require 'yaml'
+require 'growl'
 
+credentials = YAML.load_file(File.join('config','config.yml'))
+xml = RestClient.get(credentials['credentials'])
+json = Crack::XML.parse(xml)
 
+# json['feed']['entry'].each do |failed_build|
+# end
 
-json = Crack.parse(xml)
-
-puts json.inspect
+failed_build = json['feed']['entry'].first
+puts Date.parse(failed_build['published']).inspect
+Growl.notify_error("The build was broken #{Time.parse(failed_build['published']).strftime('at %I:%M %P on %m/%d/%Y')}", :sticky => true)
 
 
 
