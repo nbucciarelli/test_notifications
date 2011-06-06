b991c39b65d8b610969ee4506f57739707bf165c
Nick <nick@jackrussellsoftware.com>
2011-06-06 15:46:23 -0400

    Event machine implemented.

--- app/app.rb
+++ app/app.rb
@@ -2,17 +2,46 @@ require 'rest-client'
 require 'crack'
 require 'yaml'
 require 'growl'
+require 'eventmachine'
 
-credentials = YAML.load_file(File.join('config','config.yml'))
-xml = RestClient.get(credentials['credentials'])
-json = Crack::XML.parse(xml)
+@credentials = YAML.load_file(File.join('config','config.yml'))
 
-# json['feed']['entry'].each do |failed_build|
+# latest_global_status = Crack::XML.parse(RestClient.get(credentials['credentials']))['feed']['entry'].first
+# unless latest_global_status['title'].include? 'stable'
+#   Growl.notify_error("The build was broken #{Time.parse(latest_global_status['published']).strftime('at %I:%M %P on %m/%d/%Y')}", :sticky => true)
 # end
+def most_recent_non_aborted_status(i=0)
+  feed = return_feed
+  if i < feed.size - 1
+    status = feed[i]
+    if status['title'].include? 'aborted'
+      i = i + 1
+      most_recent_non_aborted_status(i)
+    else
+      return status
+    end
+  else
+    return []
+  end
+end
 
-failed_build = json['feed']['entry'].first
-puts Date.parse(failed_build['published']).inspect
-Growl.notify_error("The build was broken #{Time.parse(failed_build['published']).strftime('at %I:%M %P on %m/%d/%Y')}", :sticky => true)
+def return_feed
+  Crack::XML.parse(RestClient.get(@credentials['credentials']))['feed']['entry']
+end
+
+latest_global_status = nil
+puts latest_global_status.inspect
+EventMachine::run do
+  EM.add_periodic_timer(3) do
+    new_build = most_recent_non_aborted_status
+    if latest_global_status != new_build
+      if new_build['title'].include?('back to normal') || new_build['title'].include?('broken')
+        Growl.notify_error("The build was broken #{Time.parse(new_build['published']).strftime('at %I:%M %P on %m/%d/%Y')}", :sticky => true)
+      end
+      latest_global_status = new_build
+    end
+  end
+end
 
 
 
